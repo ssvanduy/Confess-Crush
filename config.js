2e63072c07dea34e8e741b9cc5bd975bcd5077f3
ssvanduy <92796934+ssvanduy@users.noreply.github.com>
2021-10-20 12:37:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Chúc Bạn Như 20/10 vui vẻ nha !!!",
+    introTitle: 'Đừng hiểu nhầm nha, chỉ đơn giản là muốn chúc bà thôi !',
+    introDesc: `Chúc bạn có một ngày 20/10 vui vẻ nha!
+    Chúc luôn những người bên cạnh bạn được hạnh phúc.
+    Sau ngày có gặp lại tui thì nhớ cười cái nhé! hihi.`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Chơi một trò game show nhẹ nhẹ hỉ !!Bà có thích được chúc như này không !🥰',
+    desc: '',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question: 'Hihi cảm ơn nha, thế năm sau lại chúc nữa nhá !',
+    btnReply: 'Có ý kiến gì thì nhắn vào đây! <3',
+    reply: 'Trên thế giới hơn 7 tỉ người, có mình ông là đẹp trai nhất!...',
+    mess: 'HAHA, ai cũng nói vậy hết má!',
+    messDesc: 'Hẹn tết gặp nha, nhớ đi họp lớp nha bà, chứ không cô lại bảo có tui bà không đi :(.',
+    btnAccept: 'Tặng bà nè !',
+    messLink: 'https://i.pinimg.com/564x/75/7f/56/757f5669734347fb6f3c09ad7eb8b1cb.jpg' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
