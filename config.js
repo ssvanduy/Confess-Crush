def11d57ce4ccc08067898b4bbfa0f744e786222
ssvanduy <92796934+ssvanduy@users.noreply.github.com>
2023-07-31 15:15:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Chúc Bạn Như 20/10 vui vẻ nha !!!",
-    introTitle: 'Đừng hiểu nhầm nha, chỉ đơn giản là muốn chúc bà thôi !',
-    introDesc: `Chúc bạn có một ngày 20/10 vui vẻ nha!
-    Chúc luôn những người bên cạnh bạn được hạnh phúc.
-    Sau ngày có gặp lại tui thì nhớ cười cái nhé! hihi.`,
-    btnIntro: '^^Thử bấm đi biết đâu có quà!^^',
-    title: 'Chơi một trò game show nhẹ nhẹ hỉ !!Bà có thích được chúc như này không !🥰',
+    titleWeb: "Chúc em sinh nhật VUI VẺ !!!",
+    introTitle: 'Anh Chỉ muốn nói là anh xin lỗi vì thời gian qua, a tệ quá!',
+    introDesc: `Cơ mà vẫn chúc em sinh nhật vui vẻ nè!
+    Chúc luôn gặp những điều tốt đẹp nhất.
+    Đừng có buồn chuyện gì nữa có gì thì tâm sự với anh, nhớ nha.`,
+    btnIntro: '^^Có quà đó đừng bấm!^^',
+    title: 'Muốn nhận quà thì hết giận đi cái đã, đố em bấm được không !🥰',
     desc: '',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Em hết giận a rồi!',
     btnNo: 'Không nha :3',
-    question: 'Hihi cảm ơn nha, thế năm sau lại chúc nữa nhá !',
-    btnReply: 'Có ý kiến gì thì nhắn vào đây! <3',
-    reply: 'Trên thế giới hơn 7 tỉ người, có mình ông là đẹp trai nhất!...',
-    mess: 'HAHA, ai cũng nói vậy hết má!',
-    messDesc: 'Hẹn tết gặp nha, nhớ đi họp lớp nha bà, chứ không cô lại bảo có tui bà không đi :(.',
-    btnAccept: 'Tặng bà nè !',
-    messLink: 'https://i.pinimg.com/564x/75/7f/56/757f5669734347fb6f3c09ad7eb8b1cb.jpg' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question: 'HAHA, sao em thắng được anh :)))) !',
+    btnReply: 'Chưa xong, nhắn gì đó vào đây đã! Gõ đại đi a biết em thích gì!<3',
+    reply: 'Em thích anh Duy... HAHA lại bị dụ rồi :) bấm vào bên dưới đi tới quà rồi đó !  ',
+    mess: 'HAHA, biết mà!',
+    messDesc: 'ANh biết bao nhiêu lời xin lỗi cũng không đủ cả, nhưng mong em bỏ qua cho anh <3.',
+    btnAccept: 'Tặng em !',
+    messLink: 'https://www.facebook.com/profile.php?id=100004851586370' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
